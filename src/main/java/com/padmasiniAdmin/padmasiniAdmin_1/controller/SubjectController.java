48ed52a459e4a575f2dfdbee421e90e1322f671d
Sneka-Padmasini <sneka_s@padmasini.com>
2025-11-14 12:51:07 +0530

    Update SubjectController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/SubjectController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/SubjectController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import com.padmasiniAdmin.padmasiniAdmin_1.model.Subject;
 import com.padmasiniAdmin.padmasiniAdmin_1.repository.SubjectRepository;
