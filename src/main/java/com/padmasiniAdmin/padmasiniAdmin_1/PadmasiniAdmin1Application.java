41a82d33fdba2bec9e0e2640936950cbde47ee36
Sneka-Padmasini <sneka_s@padmasini.com>
2026-01-03 16:02:22 +0530

    Enable scheduling and add CORS configurations

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/PadmasiniAdmin1Application.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/PadmasiniAdmin1Application.java
@@ -1,12 +1,15 @@
 package com.padmasiniAdmin.padmasiniAdmin_1;
+import java.time.LocalTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PadmasiniAdmin1Application {
 
     public static void main(String[] args) {
@@ -23,6 +26,7 @@ public class PadmasiniAdmin1Application {
                             "https://d2i6kp2bub3ddu.cloudfront.net",
                             "https://padmasini.com/Learnforward/",
                             "http://localhost:5173",
+                            "http://localhost:5173/Learnforward/",
                             "https://padmasini.com",
                             "http://localhost:5174",
                             "https://studentfrontendpage.netlify.app",
